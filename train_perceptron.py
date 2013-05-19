02de888d74308df6827cb87e4dc84ed2ebf9de6b
Yoshinari Fujinuma <y_fujinuma@nii.ac.jp>
2013-05-19 21:56:35 +0900

    second

--- train_perceptron.py
+++ train_perceptron.py
@@ -4,7 +4,6 @@
 def update_weights(weight_dic, phi, y):
   for name in phi:
     weight_dic[name] += phi[name]*y
-    #print name, weight_dic[name]
     # if y = -1, smaller
     # if y = 1, bigger
 
@@ -12,22 +11,15 @@ def learn(input_file):
   weight_dic = {}
   #assume that the first element is the y
   for line in open(input_file, "r"):
-    #print line
     temp = line[:-1].split('\t')
-    #print line
     y = int(temp.pop(0))
-    #print y
     for x in temp[0].split():
       if x not in weight_dic:
         weight_dic[x] = 0 # initializing weights
-    #print weight_dic
     phi = create_features(temp[0])
-    #print phi
     y_pre = predict_one(weight_dic, phi)
-    #print y, y_pre
     if y != y_pre:
       update_weights(weight_dic, phi, y)
-      #if 'The' in weight_dic: weight_dic['The'] = -1
   return weight_dic
 
 def create_features(sentence):
@@ -51,12 +43,7 @@ def predict_one(weight_dic, phi):
   else: return -1
 
 def predict_all(model_file, input_file):
-  #train_file = open(model_file, "r")
   weight_dic = learn(model_file) 
-  #for line in file_input:
-  #  word, weight = line[:-1].split() 
-  #  weight_dic[word] = int(weight)
-
   
   for line in open(input_file, "r"):
     phi = create_features(line[:-1])
